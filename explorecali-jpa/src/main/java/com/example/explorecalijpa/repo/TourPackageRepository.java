6d2e8faa3bd0a04a8b05ce93fab57e4bfbbc62c5
sameera-87 <150885400+sameera-87@users.noreply.github.com>
2025-09-19 13:49:21 +0000

    repo added

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.explorecalijpa.model.TourPackage;
+
+public class TourPackageRepository extends JpaRepository<TourPackage, String> {
+
+}
